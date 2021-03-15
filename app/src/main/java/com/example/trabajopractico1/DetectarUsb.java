45497ea20a640a83387ea2fe01083acd60db70bc
Lucila Lillo <lucilaslillo@gmail.com>
2021-03-15 18:38:50 -0300

    modificacion para el TP1. ahora llama al 911

---
+++ app/src/main/java/com/example/trabajopractico1/DetectarUsb.java
@@ -0,0 +1,21 @@
+package com.example.trabajopractico1;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.widget.Toast;
+
+public class DetectarUsb extends BroadcastReceiver {
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context,"Se desconecto el USB, llamar al 911",Toast.LENGTH_LONG).show();
+        Intent i = new Intent(Intent.ACTION_CALL);
+        i.setData(Uri.parse("tel:" + "911"));
+        context.startActivity(i);
+    }
+
+
+}
--- app/src/main/java/com/example/trabajopractico1/MainActivity.java
+++ app/src/main/java/com/example/trabajopractico1/MainActivity.java
@@ -9,16 +9,16 @@ import android.os.Build;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-     private MensajeRecibido mr;
+     private DetectarUsb mr;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //permisos
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
-           && checkSelfPermission(Manifest.permission.RECEIVE_SMS)
+           && checkSelfPermission(Manifest.permission.CALL_PHONE)
                 != PackageManager.PERMISSION_GRANTED) {
-               requestPermissions(new String[]{Manifest.permission.RECEIVE_SMS},1000);
+               requestPermissions(new String[]{Manifest.permission.CALL_PHONE},1000);
         }
 
     }
@@ -26,8 +26,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        mr = new MensajeRecibido();
-        registerReceiver(mr, new IntentFilter("android.provider.Telephony.SMS_RECEIVED"));
+        mr = new DetectarUsb();
+        registerReceiver(mr, new IntentFilter("android.hardware.usb.action.USB_STATE"));
     }
 
     @Override
